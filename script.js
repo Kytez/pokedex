fa0a12745d914e0428b1b58e645c1630e8b5bcd8
Marcel Luwinski <marcel.luwinski@gmail.com>
2024-03-16 10:54:14 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,31 @@
+async function init() {
+    await loadPokemon();
+    renderPokemonInfoHeaderDetailView();
+}
+
+let currentPokemon;
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+
+    console.log(currentPokemon);
+}
+
+function renderPokemonInfoHeaderDetailView() {
+    let pokemonName = currentPokemon['name'];
+    let pokemonTypes = currentPokemon['types'];
+
+    document.getElementById('pokemonName').innerHTML = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+
+    for (let i = 0; i < pokemonTypes.length; i++) {
+        const type = pokemonTypes[i]['type']['name'];
+        
+        document.getElementById('pokemonTypes').innerHTML += `
+            <span class="pokemonType">${type.charAt(0).toUpperCase() + type.slice(1)}</span>
+        `;
+    }
+    
+}
+
